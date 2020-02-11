b7b7d354c4a394a3febab30b773d1c9f407ee8bc
Arjav <arjavpatni@hotmail.com>
2020-02-11 12:13:11 +0530

    Data Binding methods calls, onClickListener for listLayouts.

---
+++ app/src/main/java/com/arjavp/dogs/view/DogClickListener.kt
@@ -0,0 +1,7 @@
+package com.arjavp.dogs.view
+
+import android.view.View
+
+interface DogClickListener {
+    fun onDogClicked(v: View)
+}
--- app/src/main/java/com/arjavp/dogs/view/DogsListAdapter.kt
+++ app/src/main/java/com/arjavp/dogs/view/DogsListAdapter.kt
@@ -1,14 +1,17 @@
 package com.arjavp.dogs.view
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.arjavp.dogs.R
 import com.arjavp.dogs.databinding.ItemDogBinding
 import com.arjavp.dogs.model.DogBreed
+import kotlinx.android.synthetic.main.item_dog.view.*
 
-class DogsListAdapter(val dogsList: ArrayList<DogBreed>): RecyclerView.Adapter<DogsListAdapter.DogViewHolder>() {
+class DogsListAdapter(val dogsList: ArrayList<DogBreed>): RecyclerView.Adapter<DogsListAdapter.DogViewHolder>(), DogClickListener {
 
     fun updateDogList(newDogsList: List<DogBreed>){
         dogsList.clear()
@@ -33,16 +36,24 @@ class DogsListAdapter(val dogsList: ArrayList<DogBreed>): RecyclerView.Adapter<D
 
     override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
         holder.view.dog = dogsList[position]// this populates the declared "dog" variable in item_dog.xml
+        holder.view.listener = this //here this refers to this Adapter inheriting from DogClickListener
         //populates textViews automatically for name and lifeSpan, refer item_dog.xml
-
-
+    }
+    //onClickListener substitute as DataBinding, fun declared in interface DogClickListener
+    //listener added to OnClick in xml file.
+    override fun onDogClicked(v: View) {
+        val uuid = v.dogId.text.toString().toInt()
+        val action = ListFragmentDirections.actionDetailFragment()
+        action.dogUuid = uuid
+        Navigation.findNavController(v).navigate(action)
+    }
+}
+// Old way:
 //        holder.view.name.text=dogsList[position].dogBreed
 //        holder.view.lifespan.text=dogsList[position].lifeSpan
 //        holder.view.setOnClickListener {
 //            val action = ListFragmentDirections.actionDetailFragment()
 //            action.dogUuid = dogsList[position].uuid
-//            Navigation.findNavController(it).navigate(ListFragmentDirections.actionDetailFragment())
+//            Navigation.findNavController(it).navigate(action)
 //        }
 //        holder.view.imageView.loadImage(dogsList[position].imageUrl, getProgressDrawable(holder.itemView.imageView.context))
-    }
-}
